8b90bd92dd2e8e200ab425f974a7b93d612dd670
grantfogle <grantjfogle@gmail.com>
2023-10-10 20:55:38 -0600

    adding blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <section className="h-96 bg-white text-black">
+            <h1 className="text-xl">This is the blog page</h1>
+        </section>
+    );
+}
